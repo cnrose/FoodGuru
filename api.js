1352dca52d7722c335cbd558dea074b62577ce24
Jessica Thompson <champagneponcey@lawn-143-215-53-129.lawn.gatech.edu>
2017-06-15 19:16:48 -0400

    jess1

---
+++ api.js
@@ -0,0 +1,10 @@
+function dirtyTrail(searchString) {
+	var trailURL = "https://trailapi-trailapi.p.mashape.com/?";url += '?' + $.param({'api-key': "IjC5nh65gVmshOq8obE7EgpkGZzyp1qHyQujsnUDDczZBYs4w8"});
+
+	$.ajax({
+		url: trailURL,
+		method: 'GET',
+	}).done(function(response){
+		console.log(response.docs.keywords);
+	});
+}
