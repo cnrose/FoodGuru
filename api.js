899b770a5de89941a0ef6617f2cfb4d3932d7836
Jessica Thompson <champagneponcey@Jessicas-MacBook-Air.local>
2017-06-19 19:45:37 -0400

    wiki api

--- api.js
+++ api.js
@@ -1,21 +1,18 @@
-<<<<<<< HEAD
-function dirtyBrew(searchString) {
-	var brewURL = "http://api.brewerydb.com/v2/search?q=" + beerType + "&type=beer&key=7c2d81a2f0ae659b839207d3897e9ac1";
-	
-	$.ajax({
-		url: brewURL,
-		method: 'GET',
-	}).done(function(data){
-		console.log();
-=======
-function dirtyTrail(searchString) {
-	var trailURL = "https://trailapi-trailapi.p.mashape.com/?";url += '?' + $.param({'api-key': "IjC5nh65gVmshOq8obE7EgpkGZzyp1qHyQujsnUDDczZBYs4w8"});
 
-	$.ajax({
-		url: trailURL,
-		method: 'GET',
-	}).done(function(response){
-		console.log(response.docs.keywords);
->>>>>>> b5505a00c90d006aba278901f865b4b0e15c05ae
-	});
-}
+	var wikiURL = "https://en.wikipedia.org/w/api.php";
+	wikiURL += '?' + $.param({
+    'action' : 'opensearch',
+    'search' : 'your_search_term',
+    'prop'  : 'revisions',
+    'rvprop' : 'content',
+    'format' : 'json',
+    'limit' : 10
+});
+
+ $.ajax( {
+    url: wikiURL,
+    dataType: 'jsonp',
+    success: function(data) {
+       console.log(data);
+    }
+} );
