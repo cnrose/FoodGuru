b22f2a7e305fb9229decc71c536b779a11fce429
Jessica Thompson <champagneponcey@lawn-143-215-53-129.lawn.gatech.edu>
2017-06-27 19:27:24 -0400

    jess2

--- api.js
+++ api.js
@@ -1,39 +1,30 @@
 $(document).ready(function() {
 
-<<<<<<< HEAD
-	var eatMe = $("#searchTerm").text();
+  var eatMe = $("#searchTerm").text();
 
-	var queryURL = "https:www.googleapis.com/youtube/v3/search?&part=snippet&q=quinoa&key=AIzaSyCqtkizKR5dTv4AP90rXLCGNG9-LLIrG_Y";
+  var queryURL = "https:www.googleapis.com/youtube/v3/search?&part=snippet&q=quinoa&key=AIzaSyCqtkizKR5dTv4AP90rXLCGNG9-LLIrG_Y";
 
-	$.ajax ({
-	    url: queryURL,
-	    method: "GET"
-	}).done(function(response){
+  $.ajax ({
+    url: queryURL,
+    method: "GET"
+  }).done(function(response){
 
-		"kind": "youtube#thumbnailSetResponse",
- 	 	"etag": etag,
-  		"items": [
-    		thumbnail resource
-  		]
+    for (var i = 0; i < response.items.length; i++) {
+      var item = response.items[i];
+      var videoTitle = item.snippet.title;
+      var videoId = item.id.videoId;
+      var imageURL = item.snippet.thumbnails.default.url;
+      var videoThumb = '<pre><img id="thumb" src=" '+imageURL+' " alt="No Image Available." style="width:204px;height:128px"></pre>';
 
-	    for (var i = 0; i < response.items.length; i++) {
-	      var item = response.items[i];
-	      var videoTitle = item.snippet.title;
-	      var videoId = item.id.videoId;
-	      var imageURL = item.snippet.thumbnails;
-	      var videoThumb = '<img id="thumb" src=" '+ imageURL +' " alt="No Image Available.">';
+      console.log(item.snippet.title + " " + item.id.videoId);
 
-	      console.log(item.snippet.title + " " + item.id.videoId);
+      var videoUrl = "https://www.youtube.com/embed/" + videoId;
 
-	      var videoUrl = "https://www.youtube.com/embed/" + videoId;
+      $("#video" + i).attr("src", videoUrl);
 
-	      $("#video" + i).attr("src", videoThumb);
-
-	    };
-
-		  // $('.carousel').carousel();
-	  
-	});  
+    };
+  
+  });  
 	
 	var wikiURL = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=500&titles=" + eatMe + "&format=json";
 
@@ -67,10 +58,9 @@ $(document).ready(function() {
 		       	$("#searchResults").html(result.extract);
         	};
 
-    	};
+    	}
 
  	});
-
 
     var config = {
         apiKey: "AIzaSyAIezGr773BQU-TDtuupeccp2gL_OFkuZ4",
@@ -104,32 +94,3 @@ $(document).ready(function() {
     });
 
 });
-
-=======
-
-  var queryURL = "https:www.googleapis.com/youtube/v3/search?&part=snippet&q=quinoa&key=AIzaSyCqtkizKR5dTv4AP90rXLCGNG9-LLIrG_Y";
-
-  $.ajax ({
-    url: queryURL,
-    method: "GET"
-  }).done(function(response){
-
-    for (var i = 0; i < response.items.length; i++) {
-      var item = response.items[i];
-      var videoTitle = item.snippet.title;
-      var videoId = item.id.videoId;
-      var imageURL = item.snippet.thumbnails.default.url;
-      var videoThumb = '<pre><img id="thumb" src=" '+imageURL+' " alt="No Image Available." style="width:204px;height:128px"></pre>';
-
-      console.log(item.snippet.title + " " + item.id.videoId);
-
-      var videoUrl = "https://www.youtube.com/embed/" + videoId;
-
-      $("#video" + i).attr("src", videoUrl);
-
-    };
-  
-  })  
-
-})
->>>>>>> a5b5b8b6fe15bd1734caba07d86500d779570231
