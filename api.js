fc4bdac1b52d37c67f7d5d1a4da76c60b1ccb589
Jessica Thompson <champagneponcey@lawn-143-215-53-129.lawn.gatech.edu>
2017-06-27 18:57:47 -0400

    most up-to-date from jess

--- api.js
+++ api.js
@@ -1,9 +1,44 @@
+$(document).ready(function() {
+
 	var eatMe = $("#searchTerm").text();
+
+	var queryURL = "https:www.googleapis.com/youtube/v3/search?&part=snippet&q=quinoa&key=AIzaSyCqtkizKR5dTv4AP90rXLCGNG9-LLIrG_Y";
+
+	$.ajax ({
+	    url: queryURL,
+	    method: "GET"
+	}).done(function(response){
+
+		"kind": "youtube#thumbnailSetResponse",
+ 	 	"etag": etag,
+  		"items": [
+    		thumbnail resource
+  		]
+
+	    for (var i = 0; i < response.items.length; i++) {
+	      var item = response.items[i];
+	      var videoTitle = item.snippet.title;
+	      var videoId = item.id.videoId;
+	      var imageURL = item.snippet.thumbnails;
+	      var videoThumb = '<img id="thumb" src=" '+ imageURL +' " alt="No Image Available.">';
+
+	      console.log(item.snippet.title + " " + item.id.videoId);
+
+	      var videoUrl = "https://www.youtube.com/embed/" + videoId;
+
+	      $("#video" + i).attr("src", videoThumb);
+
+	    };
+
+		  // $('.carousel').carousel();
+	  
+	});  
 	
-	var wikiURL = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=500&titles=" + eatMe +"&format=json";
+	var wikiURL = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=500&titles=" + eatMe + "&format=json";
 
 
     function wikiWiki() {
+
 		wikiURL += '?' + $.param({
 		   'action' : 'query',
 		   'search' : $("#searchTerm"),
@@ -11,24 +46,61 @@
 		   'format' : 'json',
 		   'limit' : 10
 		});
+
 	};
 
 	$("#searchTerm").empty();
 
- $.ajax( {
-    url: wikiURL,
-    dataType: 'jsonp',
-    success: function(response) {
-       console.log(response);
+ 	$.ajax( {
+	    url: wikiURL,
+	    dataType: 'jsonp',
+	    success: function(response) {
+	       console.log(response);
 
-       for (var prop in response.query.pages) {
-       	console.log(prop);
+	        for (var prop in response.query.pages) {
+		       	console.log(prop);
 
-       	var result = response.query.pages[prop];
-       	console.log(result);
+		       	var result = response.query.pages[prop];
+		       	console.log(result);
 
-       	$("#searchResults").html(result.extract);
-       };
-    }
-} );
+		       	$("#searchResults").html(result.extract);
+        	};
+
+    	};
+
+ 	});
+
+
+    var config = {
+        apiKey: "AIzaSyAIezGr773BQU-TDtuupeccp2gL_OFkuZ4",
+        authDomain: "food-guru-33241.firebaseapp.com",
+        databaseURL: "https://food-guru-33241.firebaseio.com",
+        projectId: "food-guru-33241",
+        storageBucket: "food-guru-33241.appspot.com",
+        messagingSenderId: "810775232856"
+    };
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
+
+    var ingredientName = "";
+
+    $("#add-ingredient-btn").on("click", function(event) {
+        event.preventDefault();
+        ingredientName = $("#ingredient-name-input").val().trim();
+        database.ref().push({
+            name: ingredientName,
+        });
+
+        database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+            console.log(childSnapshot.val());
+            var ingredientName = childSnapshot.val().name;
+            console.log(ingredientName);
+        
+            $("#ingredient-name-input").html(childSnapshot.val().ingredientName);
+
+        });
+    });
+
+});
 
