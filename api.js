658f7f53fbf2b2f48cfbdc086852aef064460fee
Chase Eidam <chaseeidam@gmail.com>
2017-07-06 21:05:53 -0400

    search bar needs to be added

---
+++ api.js
@@ -0,0 +1,128 @@
+$(document).ready(function() {
+  //initializing firebase
+  var config = {
+        apiKey: "AIzaSyAIezGr773BQU-TDtuupeccp2gL_OFkuZ4",
+        authDomain: "food-guru-33241.firebaseapp.com",
+        databaseURL: "https://food-guru-33241.firebaseio.com",
+        projectId: "food-guru-33241",
+        storageBucket: "food-guru-33241.appspot.com",
+        messagingSenderId: "810775232856"
+    };
+
+   firebase.initializeApp(config);
+    //universal variables
+    var database = firebase.database();
+    var ingredientName = "";
+  
+ var eatMe;
+
+ // Test the input and retrieve results
+  $("#add-ingredient-btn").on("click", function(){
+    event.preventDefault();
+    eatMe = $("#ingredient-name-input").val().trim();
+
+   if(searchValidation(eatMe)){
+      youTubeCall();
+      wikiCall();
+      recordSearchTerm(eatMe);
+    } else {
+      $("#ingredient-name-input").val("");
+      
+    }
+    //backgroundChange();
+    //
+  });
+
+
+ //change background picture function
+  function backgroundChange() {
+    $("body").css("background-image", 'url("assets/images/concrete-texture.png")');
+  }
+
+ //YouTube API Call & iFrame creation
+  function youTubeCall() {
+      
+   $(".videoResults").empty();
+
+   var queryURL = "https:www.googleapis.com/youtube/v3/search?&part=snippet&q=" + eatMe + "+recipe&key=AIzaSyCqtkizKR5dTv4AP90rXLCGNG9-LLIrG_Y";
+  
+    $.ajax ({
+      url: queryURL,
+      method: "GET"
+    }).done(function(response){
+      for (var i = 0; i < response.items.length; i++) {
+      
+       var item = response.items[i];
+        var videoId = item.id.videoId;
+
+       console.log(item.snippet.title + " " + item.id.videoId);
+        var videoUrl = "https://www.youtube.com/embed/" + videoId;
+        var videoNum = "video" + i;
+        $("#video" + i).attr("src", videoUrl);
+
+       //iFrame creation and appending to results div
+        var videoPlayer = $("<iframe class='video w100' width='640' height='360' src='#' frameborder='0' allowfullscreen style='margin:20px'>");
+        
+       videoPlayer.attr("id", videoNum);
+
+       videoPlayer.attr("src", videoUrl);
+
+       $(".videoResults").append(videoPlayer);
+
+
+     };
+    
+   });  
+ };
+
+ //mediaWiki call & results display
+  function wikiCall() {
+    var wikiURL = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=500&titles=" + eatMe + "&format=json";
+    function wikiWiki() {
+      wikiURL += '?' + $.param({
+        'action' : 'query',
+        'search' : $("#ingredient-name-input"),
+        'prop'  : 'info',
+        'format' : 'json',
+        'limit' : 10
+      });
+    };
+
+   $.ajax({
+      url: wikiURL,
+      dataType: 'jsonp',
+      success: function(response) {
+        console.log(response);
+        for (var prop in response.query.pages) {
+          console.log(prop);
+          var result = response.query.pages[prop];
+          console.log(result);
+          $("#searchResults").html(result.extract);
+        };
+      }
+    });
+  }
+  
+ //logging search term into Firebase
+  function recordSearchTerm(ingredientName) {
+
+   database.ref().push({
+      name: ingredientName
+    });
+
+   database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+      var ingredientName = childSnapshot.val().name;
+      $("#ingredient-name-input").html(childSnapshot.val().ingredientName);
+    });
+  };
+
+ // Validate that we have only 1 alpha character word and it is less than 30 characters long.
+  function searchValidation(searchText) {
+      var letters = /^[A-Za-z]+$/;
+      if(searchText.match(letters) && searchText.length < 30){
+        return true;
+      } else {
+        return false;
+      }
+  };
+});
